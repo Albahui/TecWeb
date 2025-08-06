ece7a56e05db2d797ce1a7ea24157e1ff58df512
Alba <149021742+Albahui@users.noreply.github.com>
2025-08-06 16:19:33 +0200

    Delete sonoerasmus/connessione.php

--- sonoerasmus/connessione.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("pgsql:host=localhost;dbname=sonoerasmus", "postgres", "diana");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Errore: " . $e->getMessage();
-}
-?>
